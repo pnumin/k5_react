2df4117aa3550b377a1fa46fe71a6017c867df37
pnumin <pnumin@pusan.ac.kr>
2023-11-28 13:02:43 +0900

    Tailwindcss

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import './App.css';
 // import LogoMain from './01/LogoMain';
 // import ClockMain from './02/ClockMain';
 // import FrontEnd from './03/FrontEnd';
-import Lotto from './05/Lotto';
+// import Lotto from './05/Lotto';
+import BoxOffice from './06/BoxOffice';
 
 function App() {
   return (     
     // <LogoMain />    
     // <ClockMain />
     // <FrontEnd />  
-    <Lotto />
+    // <Lotto />
+    <BoxOffice />
   );
 }
 
---
+++ src/UI/TailH1.js
@@ -0,0 +1,7 @@
+export default function TailH1({title}) {
+  return (
+    <div className="text-4xl text-sky-900 font-sans font-bold">
+      {title}
+    </div>
+  )
+}
