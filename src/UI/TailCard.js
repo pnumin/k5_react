8cd8d02f826278658cd25a552c5389b7e8fbbca6
pnumin <pnumin@pusan.ac.kr>
2023-12-20 09:29:58 +0900

    card컴포넌트

--- src/UI/TailCard.js
+++ src/UI/TailCard.js
@@ -1,19 +1,23 @@
 
 export default function TailCard({ imgSrc, title, subtitle, tags }) {
+  let sps = tags.split(',') ;
+  sps = sps.map((item, idx) => 
+    <span key={`sp${idx}`} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+      {item}
+    </span>
+  )
   return (
-    <div>
-      <div>
-        <img src={imgSrc} alt={title} />
-      </div>
-      <div className="space-y-8 text-5xl text-shadow-lg font-bold text-gray-900 dark:text-white shadow-red-500/50">
-        {title}
-      </div>
-      <div>
-        {subtitle}
-      </div>
-      <div>
-        {tags}
-      </div>
+    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+      <img class="w-full" src={imgSrc} alt={title} />
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2">{title}</div>
+          <p class="text-gray-700 text-base">
+            {subtitle}
+          </p>
+        </div>
+        <div class="px-6 pt-4 pb-2">
+          {sps}          
+        </div>
     </div>
   )
 }
